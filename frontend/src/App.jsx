9ee81219f98d8d1af560b4503a75c2b3a9a49dda
ycydro <180024097+ycydro@users.noreply.github.com>
2025-09-24 19:45:21 +0800

    chore: install react-router-dom (real)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import { Button } from "@/components/ui/button";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Test from "./pages/Test";
 
-function App() {
+const App = () => {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/test" element={<Test />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;

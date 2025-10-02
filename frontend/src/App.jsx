107faebd2a7adb33957bb6cbcaa3fa0cc82e8df1
ycydro <180024097+ycydro@users.noreply.github.com>
2025-10-02 22:07:04 +0800

    chore: wrap app in queryclientprovider and authprovider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,20 @@
 import { Button } from "@/components/ui/button";
 import { BrowserRouter as Router } from "react-router-dom";
-import Test from "./pages/Test";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AppRoutes from "./routes/AppRoutes";
+import { AuthProvider } from "./context/AuthContext";
+
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <Router>
-      <AppRoutes />
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <AuthProvider>
+          <AppRoutes />
+        </AuthProvider>
+      </Router>
+    </QueryClientProvider>
   );
 };
 

a2c7aa1f308da0217af193422978d7660afab366
ycydro <180024097+ycydro@users.noreply.github.com>
2025-11-20 21:56:18 +0800

    style(ui): shadcn component improvements (button & dialog)

--- frontend/src/components/ui/button.jsx
+++ frontend/src/components/ui/button.jsx
@@ -13,7 +13,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-white hover:bg-destructive/80 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
-          "border border-primary bg-[#fefefe] hover:bg-primary hover:text-white dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
+          "border border-primary bg-background hover:bg-primary hover:text-white dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         "sdo-secondary":

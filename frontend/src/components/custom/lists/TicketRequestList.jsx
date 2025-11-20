6e37fd6855143be44ca88be5f4c0377f34d1696c
ycydro <180024097+ycydro@users.noreply.github.com>
2025-11-20 21:27:20 +0800

    refactor: add skeleton loading latest ticket request list

--- frontend/src/components/custom/lists/TicketRequestList.jsx
+++ frontend/src/components/custom/lists/TicketRequestList.jsx
@@ -1,11 +1,26 @@
 import { Badge } from "@/components/ui/badge";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useTickets } from "@/hooks/queries/ticket/useTickets";
 
-export const TicketRequestList = ({ tickets }) => {
+export const TicketRequestList = () => {
+  const { data: tickets, isLoading: isLatestTicketsLoading } = useTickets(
+    { pageIndex: 0, pageSize: 6 },
+    "",
+    {}
+  );
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-      {tickets.map((ticket) => (
-        <TicketRequestCard key={ticket.id} ticket={ticket} />
-      ))}
+      {isLatestTicketsLoading ? (
+        Array.from({ length: 6 }).map((_, idx) => (
+          <Skeleton key={idx} className="min-w-[300px] min-h-[100px]" />
+        ))
+      ) : tickets?.data && tickets.data.length > 0 ? (
+        tickets.data.map((ticket) => (
+          <TicketRequestCard key={ticket.id} ticket={ticket} />
+        ))
+      ) : (
+        <div>No data available</div>
+      )}
     </div>
   );
 };
@@ -17,20 +32,20 @@ const TicketRequestCard = ({ ticket }) => {
       className="p-4 shadow-md border border-black/50 flex flex-col bg-[#f9f9fa] min-w-[300px]"
     >
       <div className="flex gap-2 items-center justify-between mb-2">
-        <p className="truncate text-lg font-semibold">
-          Ticket #{ticket.ticketNumber}
-        </p>
-        <Badge variant={ticket.priority.toLowerCase()}>{ticket.priority}</Badge>
+        <p className="truncate text-lg font-semibold">{ticket.ticket_code}</p>
+        <Badge>{ticket.status}</Badge>
       </div>
 
       <div className="text-sm space-y-2 mb-4">
-        <p className="text-xs text-muted-foreground">{ticket.issueType}</p>
-        <p>{ticket.description}</p>
+        {/* <p className="text-xs text-muted-foreground">{ticket.issueType}</p> */}
+        <p className="truncate" title={ticket.details}>
+          {ticket.details}
+        </p>
       </div>
 
       <div className="flex gap-3 mt-auto text-sm">
-        <p className="font-semibold">{ticket.client}</p>
-        <span className="text-gray-500">{ticket.timeAgo}</span>
+        <p className="font-semibold">{ticket.client.first_name}</p>
+        {/* <span className="text-gray-500">{ticket.timeAgo}</span> */}
       </div>
     </div>
   );
--- frontend/src/pages/Main/MainDashboard/MainDashboardPage.jsx
+++ frontend/src/pages/Main/MainDashboard/MainDashboardPage.jsx
@@ -5,16 +5,9 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import OverallSatisfactionPieChart from "../../../components/custom/charts/OverallSatisfactionPieChart";
 import TicketsByPriorityLineChart from "../../../components/custom/charts/TicketsByPriorityLineChart";
 import { TicketRequestList } from "../../../components/custom/lists/TicketRequestList";
-import { useTickets } from "@/hooks/queries/ticket/useTickets";
 import { TicketStatusCountList } from "@/components/custom/lists/TicketStatusCountList";
 
 const MainDashboardPage = () => {
-  const { data: tickets, isLoading } = useTickets(
-    { pageIndex: 0, pageSize: 23 },
-    "",
-    {}
-  );
-
   return (
     <main className="min-w-full space-y-5">
       <TicketStatusCountList />
@@ -29,11 +22,7 @@ const MainDashboardPage = () => {
           <CardTitle className="text-2xl">Latest Ticket Request</CardTitle>
         </CardHeader>
         <CardContent className="flex flex-wrap gap-2 justify-center w-full max-h-[270px] overflow-y-auto">
-          {isLoading ? (
-            <div>Ticket still loading</div>
-          ) : (
-            <TicketRequestList tickets={tickets?.data} />
-          )}
+          <TicketRequestList />
         </CardContent>
       </Card>
     </main>

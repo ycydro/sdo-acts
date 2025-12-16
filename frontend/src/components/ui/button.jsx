7367b5753f0ef7a6301b31344c006acd04fa3750
ycydro <180024097+ycydro@users.noreply.github.com>
2025-12-16 21:22:46 +0800

    style(ui): change background to inherit parent bg collor of buttons 'outline' variant

--- frontend/src/components/ui/button.jsx
+++ frontend/src/components/ui/button.jsx
@@ -13,7 +13,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-white hover:bg-destructive/80 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
-          "border border-primary bg-white hover:bg-primary hover:text-white dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
+          "border border-primary bg-inherit hover:bg-primary hover:text-white dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         "sdo-secondary":

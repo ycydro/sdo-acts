82ce4fffb17660ef11a8311b3e1949dc593aa443
ycydro <180024097+ycydro@users.noreply.github.com>
2026-03-13 10:37:01 +0800

    refactor: show a more accurate error message when email input is empty upon logging in

--- frontend/src/validations/authSchema.js
+++ frontend/src/validations/authSchema.js
@@ -1,7 +1,7 @@
 import * as z from "zod";
 
 export const loginSchema = z.object({
-  email: z.email(),
+  email: z.string().min(1, "Email is required").email("Invalid email address"),
   password: z.string().min(1, "Password is required"),
   remember: z.boolean().optional(),
 });
